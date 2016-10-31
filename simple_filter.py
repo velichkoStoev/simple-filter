00d625622b796917c6a11bd5aa74b81994f8710f
Velichko <veli4kostoev@gmail.com>
2016-10-31 17:36:01 +0200

    Improve command line args handling and help output

--- simple_filter.py
+++ simple_filter.py
@@ -3,9 +3,11 @@ from PIL import Image
 from image_filter import ImageFilter
 
 if __name__ == '__main__':
+  available_filters = ['grayscale', 'luminance', 'desaturation', 'min_decomposition', 'max_decomposition']
+
   parser = argparse.ArgumentParser(description="Providing a basic functionality for filtering an image")
-  parser.add_argument('--path', required=True, help="The path to the image file", metavar='')
-  parser.add_argument('--filter', required=True, help="The name of the filter", metavar='')
+  parser.add_argument('-p', '--path', required=True, help="The path to the image file", metavar='')
+  parser.add_argument('-f', '--filter', required=True, help="The name of the filter. Available filters: " + ', '.join(available_filters), metavar='', choices=available_filters)
   args = parser.parse_args()
 
   image = Image.open(args.path)
