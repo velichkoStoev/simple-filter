cd0bcf6e5d9551277eaceab10b2a2e3a6f638758
Velichko <veli4kostoev@gmail.com>
2016-10-31 17:16:52 +0200

    Use argparse to handle command line args

--- simple_filter.py
+++ simple_filter.py
@@ -1,10 +1,14 @@
+import  argparse
 from PIL import Image
 from image_filter import ImageFilter
 
 if __name__ == '__main__':
-  filename = 'test.jpg'
+  parser = argparse.ArgumentParser(description="Providing a basic functionality for filtering an image")
+  parser.add_argument('--path', required=True, help="The path to the image file", metavar='')
+  parser.add_argument('--filter', required=True, help="The name of the filter", metavar='')
+  args = parser.parse_args()
 
-  image = Image.open(filename)
-  image_filter = ImageFilter(image, 'grayscale')
+  image = Image.open(args.path)
+  image_filter = ImageFilter(image, args.filter)
   image_filter.applyFilter()
   image_filter.saveImage()
